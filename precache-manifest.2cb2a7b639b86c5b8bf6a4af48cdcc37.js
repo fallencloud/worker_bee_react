5f2a228a9710e2633beb804c2df07e7d98f28155
fallencloud <sharinavjones@gmail.com>
2019-05-09 04:10:13 -0400

    Updates

---
+++ precache-manifest.2cb2a7b639b86c5b8bf6a4af48cdcc37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f098d03ab76bc16f90b86e0808d441",
+    "url": "/worker_bee_react/index.html"
+  },
+  {
+    "revision": "1989ab0c102013d3e787",
+    "url": "/worker_bee_react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "80acc1fed2b6c2e35318",
+    "url": "/worker_bee_react/static/css/main.1121625a.chunk.css"
+  },
+  {
+    "revision": "1989ab0c102013d3e787",
+    "url": "/worker_bee_react/static/js/2.77c3295f.chunk.js"
+  },
+  {
+    "revision": "80acc1fed2b6c2e35318",
+    "url": "/worker_bee_react/static/js/main.1c91f027.chunk.js"
+  },
+  {
+    "revision": "fe779df530d39ec915c5",
+    "url": "/worker_bee_react/static/js/runtime~main.701f412e.js"
+  }
+]);
